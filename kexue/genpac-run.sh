754bb0ea130c2c6312f585cf9a2ed66ba1da5299
Shiman Guo <guoshim@gmail.com>
2018-03-22 15:46:39 +0800

    GFW tools

---
+++ kexue/genpac-run.sh
@@ -0,0 +1,1 @@
+genpac --pac-proxy "SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" --gfwlist-url=https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt --output="autoproxy.pac"
